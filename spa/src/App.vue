0d9b1acff53621fb8d40d684b1a5e281d8810d89
1zero96 <1zero@kakao.com>
2022-12-20 14:33:53 +0900

    Feat. App.vue 페이지 마크업
    router-view 앨리먼트와 router-link 앨리먼트를 사용하여 App.vue 페이지 마크업

---
+++ spa/src/App.vue
@@ -0,0 +1,37 @@
+<!-- 첫 화면에 표시될 내용 마크업 -->
+<template>
+  <div id="app">
+    <h1>안녕하세요!</h1>
+    <!-- 라우터 컴포넌트에 연결된 컴포넌트 렌더링 -->
+    <router-view></router-view>
+    <hr />
+    <p>
+      라우터 링크 사용: <br />
+      <router-link to="/main">메인 페이지</router-link><br />
+      <router-link to="/sub">서브 페이지</router-link>
+    </p>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
