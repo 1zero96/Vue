2bd36a5c319d3ae056d691592452bb40c8796b5d
1zero96 <1zero@kakao.com>
2022-12-20 15:04:07 +0900

    Feat. sub_page 컴포넌트 작성
    @click 이벤트 바인딩으로 fnMainPage() 메소드를 실행하는 버튼 생성
    export default 생성자와 $router 글로벌 객체의 push() 메소드를 사용해 주소 '/main'으로 이동하는 fnMainPage() 메소드 작성

---
+++ spa/src/sub_page.vue
@@ -0,0 +1,17 @@
+<!-- 서브 페이지 마크업 -->
+<template>
+  <div>
+    <h2>서브 페이지 입니다.</h2>
+    <button @click="fnMainPage">메인 페이지로 이동</button>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    // 라우터 함수로 이동할 때는 $router 글로벌 객체에 이동할 주소 입력
+    fnMainPage() {
+      this.$router.push("/main");
+    },
+  },
+};
+</script>
